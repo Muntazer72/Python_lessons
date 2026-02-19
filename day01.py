cf30535009272f4d215549331ce22c4a2998d3a7
Muntazer72 <muntazer590@gmail.com>
2026-02-19 15:46:47 +0500

    A python first lesson in cloud computing course

---
+++ day01.py
@@ -0,0 +1,36 @@
+print("Hello this is add two numbers")
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+result = n1 + n2
+
+print("Sum is:", result)
+
+print("Hello this is Substract two numbers")
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+result = n1 + n2
+
+print("After Substract:", result)
+
+
+print("Hello this is division two numbers")
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+result = n1 / n2 
+
+print("After Divison: ",result)
+
+print("Hello this is Multiplication two numbers")
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+
+result = n1 * n2 
+
+print("After Multiplication: ",result)
