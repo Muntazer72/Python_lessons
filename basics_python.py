31157dc700d515b0e726a323f720017a6b9ebccf
Muntazer72 <muntazer590@gmail.com>
2026-02-20 15:41:14 +0500

    Updated day01 and added basics_python

---
+++ basics_python.py
@@ -0,0 +1,22 @@
+
+# Assign values to variable by rules of naming a variable
+firstName = "Muntazer"
+LastName = "Mahdi"
+user_id = 10
+user_city = "Muzaffargarh"
+user_cnic = "7423987410238479832"
+user_DOB = 2005
+user_Nationality = "Pakistani"
+user_matric_marks = 1005
+user_matric_percentage = 91
+user_inter_marks = 1005
+user_inter_percentage = 91
+
+print(f"""The user first name is: {firstName}
+    The user last name is: {LastName}
+    User id is: {user_id}
+    The user city is: {user_city}
+    The user cnic is: {user_cnic}
+    The user DOB is {user_DOB} 
+    """)
+print(type(user_id))
--- day01.py
+++ day01.py
@@ -1,3 +1,5 @@
+#add two numbers
+print("Welcome to a practice based Calcuator")
 print("Hello this is add two numbers")
 
 n1 = int(input("Enter first number: "))
@@ -6,17 +8,18 @@ n2 = int(input("Enter second number: "))
 result = n1 + n2
 
 print("Sum is:", result)
-
+# substract two numbers
 print("Hello this is Substract two numbers")
 
 n1 = int(input("Enter first number: "))
 n2 = int(input("Enter second number: "))
 
-result = n1 + n2
+result = n1 - n2
 
 print("After Substract:", result)
 
 
+# divide two numbers
 print("Hello this is division two numbers")
 
 n1 = int(input("Enter first number: "))
@@ -25,7 +28,7 @@ n2 = int(input("Enter second number: "))
 result = n1 / n2 
 
 print("After Divison: ",result)
-
+# multiply two numbers
 print("Hello this is Multiplication two numbers")
 
 n1 = int(input("Enter first number: "))
